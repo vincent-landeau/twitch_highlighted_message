578a9025054c50d9a8225406d934e27e0ae6edc8
Vincent Landeau <vincent.landeau.contact@gmail.com>
2022-10-22 18:33:33 +0200

    style Highlight

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Highlight from "./Highlight";
+import './Highlight.css'
 
 class App extends React.Component {
     render() {
--- src/Highlight.js
+++ src/Highlight.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import tmi from "tmi.js";
+import {gsap} from "gsap";
 
 class Highlight extends React.Component {
     componentDidMount() {
@@ -11,41 +12,36 @@ class Highlight extends React.Component {
 
         client.on('message', (channel, tags, message, self) => {
             if (message.includes("!alert ")) {
-                document.querySelector("#content").firstChild.innerHTML = `${tags['display-name']}: ${message.replace("!alert ", "")}`;
+                document.querySelector(".username").innerHTML = tags['display-name'];
+                document.querySelector(".message").innerHTML = message.replace("!alert ", "");
                 this.showMessage()
             }
         });
     }
 
-    showMessage () {
-        let content = document.querySelector("#content");
-        content.animate([
-            { opacity: 0 },
-            { opacity: 1 }
-        ], {
-            duration: 400,
-            iterations: 1,
-        })
-        content.style.opacity = 1
-        setTimeout(this.hideMessage, 2000)
-    }
-
-    hideMessage () {
-        let content = document.querySelector("#content");
-        content.animate([
-            { opacity: 1 },
-            { opacity: 0 }
-        ], {
-            duration: 400,
-            iterations: 1,
-        })
-        content.style.opacity = 0
+    showMessage() {
+        const tl = [
+            gsap.fromTo(['.username', '.message'], {opacity: 0}, {opacity: 1, duration: .4, delay: .4}).reversed(false),
+            gsap.fromTo("hr.top", {x: "-100%"}, {x: 0, duration: .4}).reversed(false),
+            gsap.fromTo("hr.bottom", {x: "100%"}, {x: 0, duration: .4}).reversed(false),
+            gsap.fromTo(".content", {backgroundColor: "transparent"}, {backgroundColor: "rgba(0, 0, 0, 0.8)", duration: .4}).reversed(false)
+        ]
+        setTimeout(() => {
+            tl.forEach((value) => {
+                value.reversed(true)
+            })
+        }, 4000);
     }
 
     render() {
         return (
-            <div id="content">
-                <h1 className="test">{this.text}</h1>
+            <div id="hilightedMessage" className={"container"}>
+                <div className="content">
+                    <hr className={"top"}/>
+                    <h1 className="username"></h1>
+                    <p className="message"></p>
+                    <hr className={"bottom"}/>
+                </div>
             </div>
         )
     }
